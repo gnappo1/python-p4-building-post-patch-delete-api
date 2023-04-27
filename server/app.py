cc9e96fe608b4bfca8b6a01fc40b80056087b7cb
Matteo Piccini <gnappo.1@live.it>
2023-04-26 17:33:55 -0700

    done

--- server/app.py
+++ server/app.py
@@ -22,18 +22,8 @@ def index():
 @app.route('/games')
 def games():
 
-    games = []
-    for game in Game.query.all():
-        game_dict = {
-            "title": game.title,
-            "genre": game.genre,
-            "platform": game.platform,
-            "price": game.price,
-        }
-        games.append(game_dict)
-
     response = make_response(
-        games,
+        [game.to_dict() for game in  Game.query.all()],
         200
     )
 
@@ -52,18 +42,56 @@ def game_by_id(id):
 
     return response
 
-@app.route('/reviews')
+@app.route('/reviews/<int:id>', methods=['GET', 'DELETE', 'PATCH'])
+def review_by_id(id):
+    review = Review.query.filter(Review.id==id).first_or_404()
+    
+    if request.method == 'GET':
+        response = make_response(review.to_dict(), 200, {'Content-Type': 'application/json'})
+    elif request.method == 'PATCH':
+        import ipdb; ipdb.set_trace()
+        for attr in request.json:
+            setattr(review, attr, request.json.get(attr))
+        db.session.add(review)
+        db.session.commit()
+        response = make_response(review.to_dict(), 200)
+
+    elif request.method == 'DELETE':
+        db.session.delete(review)
+        db.session.commit()
+        
+        response = make_response({}, 204)
+
+    return response
+
+@app.route('/reviews', methods=['GET', 'POST'])
 def reviews():
+    if request.method == 'GET':
+        reviews = []
+        for review in Review.query.all():
+            review_dict = review.to_dict()
+            reviews.append(review_dict)
 
-    reviews = []
-    for review in Review.query.all():
-        review_dict = review.to_dict()
-        reviews.append(review_dict)
-
-    response = make_response(
-        reviews,
-        200
-    )
+        response = make_response(
+            reviews,
+            200
+        )
+    elif request.method == 'POST':
+        new_review = Review(
+            score=request.json.get("score"),
+            comment=request.json.get("comment"),
+            game_id=request.json.get("game_id"),
+            user_id=request.json.get("user_id"),
+        )
+        
+        db.session.add(new_review)
+        db.session.commit()
+        
+        response = make_response(
+            new_review.to_dict(),
+            201
+        )
+        
 
     return response
 
